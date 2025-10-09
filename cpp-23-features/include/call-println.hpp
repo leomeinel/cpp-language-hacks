dcc34c7906fcabfb79f87c591ea0c47ef8203ccb
Leopold Johannes Meinel <leo@meinel.dev>
2025-10-09 20:11:16 +0200

    Program: Add a println example

---
+++ cpp-23-features/include/call-println.hpp
@@ -0,0 +1,18 @@
+/*
+ * File: call-println.hpp
+ * Author: Leopold Meinel (leo@meinel.dev)
+ * -----
+ * Copyright (c) 2025 Leopold Meinel & contributors
+ * SPDX ID: MIT
+ * URL: https://opensource.org/licenses/MIT
+ * -----
+ */
+
+#ifndef CALL_PRINTLN_HPP
+#define CALL_PRINTLN_HPP
+
+#include <string>
+
+void call_println(const std::string &fmt);
+
+#endif // PRINTLN_HPP
---
+++ cpp-23-features/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: main.cpp
+ * Author: Leopold Meinel (leo@meinel.dev)
+ * -----
+ * Copyright (c) 2025 Leopold Meinel & contributors
+ * SPDX ID: MIT
+ * URL: https://opensource.org/licenses/MIT
+ * -----
+ */
+
+#include "include/call-println.hpp"
+
+#define OUTPUT "Hello World"
+int main(int argc, char *argv[]) {
+  call_println((OUTPUT));
+  return 0;
+}
